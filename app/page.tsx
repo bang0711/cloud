1ecd5c51946775fb0438a457d04a0f0555e48c23
bang0711 <bang07112004@gmail.com>
2023-10-29 16:10:40 +0700

    new update

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
-/* eslint-disable @next/next/no-img-element */
-
 import Link from "next/link";
 
 type Props = {};
-// async function getImages() {
-//   const res = await fetch("http://localhost:3000/api/getImages");
-//   return res.json();
-// }
+async function getImages() {
+  const res = await fetch("https://cloud-nine-ruddy.vercel.app/api/getImages");
+  return res.json();
+}
 async function HomePage({}: Props) {
-  // const images = await getImages();
+  const images = await getImages();
   return (
     <div>
-      <h1>Images</h1>{" "}
-      {/* {images.map((image: string) => (
+      <h1>Images</h1> This is image
+      {images.map((image: string) => (
         <Link href={image} key={image}>
           <img src={image} alt="" />
         </Link>
-      ))} */}
+      ))}
     </div>
   );
 }
