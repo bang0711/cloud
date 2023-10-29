2200e7a5e6e2131f90ee55de73eb0c145037615e
bang0711 <bang07112004@gmail.com>
2023-10-29 16:00:19 +0700

    new

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,16 @@ import Link from "next/link";
 
 type Props = {};
 async function getImages() {
-  const res = await fetch("http://localhost:3000/api/getImages");
+  const res = await fetch(
+    "https://cloud-git-main-bang0711.vercel.app/api/getImages"
+  );
   return res.json();
 }
 async function HomePage({}: Props) {
   const images = await getImages();
   return (
     <div>
-      <h1>Images</h1>\{" "}
+      <h1>Images</h1>{" "}
       {images.map((image: string) => (
         <Link href={image} key={image}>
           <img src={image} alt="" />
